436f912964bca39a1093e7b5aab04c0af8d717dd
zhou <343095962@qq.com>
2026-05-20 11:09:32 +0800

    feat: support plugins {} block, upgrade to 2.0.19

---
+++ plugin/src/main/java/com/meituan/android/walle/WallePluginMarker.java
@@ -0,0 +1,10 @@
+package com.meituan.android.walle;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+ * Marker interface for Walle Gradle Plugin to support plugins {} block
+ */
+public interface WallePluginMarker extends Plugin<Project> {
+}
